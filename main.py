fef2a4746c62907d077fed8f46bd7fffddf18979
NightFox <87749296+MKinG4ever@users.noreply.github.com>
2024-08-06 03:03:35 +0330

    main v1.1

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from Toolkit import Toolkit
+from Geonode import Geonode
 
 
 def main():
     """
-    proxyToolkit Module v1.0
+    proxyToolkit Module v1.1
     The MAIN function runs the proxyToolkit sample
     """
+    # Test: Toolkit Version
+    toolkit_version()
 
-    # Create an Object
+    # Test: Geonode Version
+    geonode_version()
+
+
+def toolkit_version():
+    # Create an Object from Toolkit class
     tools = Toolkit()
 
     # Import JSON File
@@ -15,6 +23,25 @@ def main():
 
     # Check the proxies from JSON File
     tools.check_the_proxies(json_all, timeout=5)
+
+
+def geonode_version():
+    # Create an Object from Geonode class (child of Toolkit)
+    tools = Geonode()
+
+    # Sample List | Toolkit
+    # json_file = tools.import_standard_json('sample.json')
+
+    # Iran Proxy List | Geonode
+    # api_fetch = tools.fetch_api(tools.generate_url())
+    api_data = tools.read_api('api_file.json')
+    api_list = tools.export_proxies(api_data)
+
+    # tools.check_the_proxies(proxy_list=json_file, timeout=5)
+    tools.check_the_proxies(proxy_list=api_list, timeout=5)
+
+    # Final presentation
+    print(tools)
 
 
 if __name__ == '__main__':
